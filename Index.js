2204cb2d3993962f1e78ed00c85474e92ec12d8c
Ikponmwosa <otasowieik@gmail.com>
2021-12-22 21:12:24 -0500

    added comments to each route describing we think they should do

--- Index.js
+++ Index.js
@@ -1,15 +1,16 @@
 /* eslint-disable no-console */
 const express = require('express')
+const { createNewPerson, getAllContacts, getContactById, uploadCSVFile, uploadContacts } =  require('./controller')
 
 const app = express()
 
-app,get('/api/contacts', getAllContacts)
+app.get('/api/contacts', getAllContacts) // displays all contacts on the database
 
-app.get('/api/contacts/:id', getContactById)
+app.get('/api/contacts/:id', getContactById) // displays one contact whose id matches the one specified in the route
 
-app.post('/api/contacts', uploadCSVFile)
+app.post('/api/contacts', uploadCSVFile) // uploads contacts using a CSV file to the database
 
-app.post('/api/contacts', uploadContacts)
+app.post('/api/contacts', createNewPerson) // uploads a contact (or contacts) using "Add Contacts" made on our UI(FrontEnd)
 
 app.all('*', (request, response) => {
   response.sendStatus(404)
