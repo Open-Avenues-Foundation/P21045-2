e26b16a044df6f9addc559488c4fc0df47b05c0c
Cat <catb0591@gmail.com>
2022-01-29 12:55:50 -0500

    Create skeleton for all text message controllers

--- controllers/textMessageControllers.js
+++ controllers/textMessageControllers.js
@@ -1,8 +1,28 @@
 const models = require('../models')
 
+const addText = () => {
+}
+
+const getAllTexts = () => {
+}
+
+const getTextById = () => {
+}
+
+const updateText = () => {
+}
+
+const deleteText = () => {
+}
+
 const sendTextMessage = async (request, response) => {
 }
 
 module.exports = {
+  addText,
+  getAllTexts,
+  getTextById,
+  updateText,
+  deleteText,
   sendTextMessage
 }
