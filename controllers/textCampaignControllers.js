15cff54b976c85bf6dd45928067c4bf1b08d8f2f
Cat <catb0591@gmail.com>
2022-01-29 12:58:42 -0500

    create skeleton of text campaign controller

---
+++ controllers/textCampaignControllers.js
@@ -0,0 +1,28 @@
+const models = require('../models')
+
+const addCampaign = () => {
+}
+
+const getAllCampaigns = () => {
+}
+
+const getCampaignById = () => {
+}
+
+const updateCampaign = () => {
+}
+
+const deleteCampaign = () => {
+}
+
+const startCampaign = () => {
+}
+
+module.exports = {
+  addCampaign,
+  getAllCampaigns,
+  getCampaignById,
+  updateCampaign,
+  deleteCampaign,
+  startCampaign
+}
