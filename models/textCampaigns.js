be4adacda31d9736c7ba7e56c98ff94ea98c96a3
Ikponmwosa <otasowieik@gmail.com>
2022-02-06 10:57:20 -0500

    updated Models and Migrations datatypes

--- models/textCampaigns.js
+++ models/textCampaigns.js
@@ -3,8 +3,8 @@ const TextCampaigns = (connection, Sequelize) => {
     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
     message: { type: Sequelize.STRING, allowNull: false },
     name: { type: Sequelize.STRING, allowNull: false },
-    timeInitiated: { type: Sequelize.DATE, allowNull: false },
-    status: { type: Sequelize.BOOLEAN, allowNull: false }
+    timeInitiated: { type: Sequelize.DATE, allowNull: true },
+    status: { type: Sequelize.ENUM('Pending', 'Sent', 'Failed'), allowNull: false }
   }, { paranoid: true })
 }
 
