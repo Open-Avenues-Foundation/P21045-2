1793f981e45fbddd2ef5474882b824393f1708f5
Hollis Jamison <hollisjamison@gmail.com>
2022-02-15 00:28:30 -0500

    fully utilizes mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
 import CSVUpload from './components/CSVUpload'
 import LandingPage from './components/LandingPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+const darkPalette = {
+  palette: {
+    mode: "dark",
+  },
+};
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path='/contact/upload' element={ <CSVUpload/>} />
-          <Route path='/' element ={ <LandingPage/>} />
-        </Routes>
+        <ThemeProvider theme={createTheme(darkPalette)}>
+          <CssBaseline>
+            <Routes>
+              <Route path='/contact/upload' element={<CSVUpload />} />
+              <Route path='/' element={<LandingPage />} />
+            </Routes>
+          </CssBaseline>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,49 +1,102 @@
-import React, { useEffect, useState } from 'react'
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  TextField,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const LandingPage = () => {
-    const [contacts, setContacts] = useState([])
-    const [matchingContacts, setMatchingContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
+  const [matchingContacts, setMatchingContacts] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    axios.get("http://localhost:1336/api/contact").then((payload) => {
+      const { data } = payload;
+      setContacts(data);
+      setMatchingContacts(data);
+    });
+  }, []);
 
-        axios.get('http://localhost:1336/api/contact').then((payload) => {
-            const { data } = payload
-            setContacts(data)
-            setMatchingContacts(data)
-        })
-    }, [])
+  const filteredContacts = (userInput) => {
+    const matchingContacts = contacts.filter((contact) => {
+      return contact.firstName
+        .toLowerCase()
+        .startsWith(userInput.toLowerCase());
+    });
 
-    const filteredContacts = (userInput) => {
-        const matchingContacts = contacts.filter(contact => {
-            return contact.firstName.toLowerCase().startsWith(userInput.toLowerCase())
-        })
+    setMatchingContacts(matchingContacts);
+  };
 
-        setMatchingContacts(matchingContacts)
-    }
+  return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid
+        container
+        item
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ margin: 3 }}
+        spacing={2}
+      >
+        <Grid item xs={12} sm={12} md={6} lg={8}>
+          <TextField
+            className="searchBox"
+            variant="outlined"
+            name="search"
+            label="Search Contact..."
+            fullWidth
+            onChange={(event) => filteredContacts(event.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} lg={2}>
+          <Link to={"/contact/upload"}>
+            <Button fullWidth variant="outlined">
+              Upload Contacts
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} lg={2}>
+          <Link to={"/campaign"}>
+            <Button fullWidth variant="outlined">
+              View Campaigns
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        item
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        xs={12}
+        sx={{ mx: 3 }}
+      >
+        {matchingContacts.map((contact) => {
+          return (
+            <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+              <Card key={contact.id}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {contact.firstName} {contact.lastName}
+                  </Typography>
+                  <Typography gutterBottom variant="body2" component="div">
+                    {contact.city}, {contact.state}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Grid>
+  );
+};
 
-    return (
-        <div>
-            <div>
-                <input className="searchBox" type="text" name="search" placeholder='Search Contact...'
-                    onChange={(event) => filteredContacts(event.target.value)}
-                />{' '}
-                <Button variant="outlined"><Link to={'/contact/upload'}>Upload Contacts</Link></Button>{' '}
-                <Button variant="outlined"><Link to={'/campaign'}>View Campaigns</Link></Button>
-            </div>
-            <div>
-                {matchingContacts.map(contact => {
-                    return (
-                        <div key={contact.id}>
-                            {contact.firstName} {contact.lastName}
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-    )
-}
-
-export default LandingPage
+export default LandingPage;
