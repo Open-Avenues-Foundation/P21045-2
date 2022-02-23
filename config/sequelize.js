9a0a99da8c31147222d0d0fd46a28855d3bb3b8f
Ikponmwosa <otasowieik@gmail.com>
2022-02-22 22:39:41 -0500

    bug fixes

--- config/sequelize.js
+++ config/sequelize.js
@@ -5,5 +5,12 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
   }
 }
